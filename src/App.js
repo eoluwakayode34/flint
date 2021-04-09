56660f7a4d1ccac4168bfeeb942c6a5eb6ccbe50
Olagundoye Emmanuel <40691457+eoluwakayode34@users.noreply.github.com>
2021-04-09 09:40:23 +0100

    feat(pixel-perfect): making design pixel perfect

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import {motion} from 'framer-motion'
 import logo from './asset/images/logo.svg'
 import star from './asset/images/star.svg'
 
-import box1 from './asset/images/footer-icon-1.svg'
-import box2 from './asset/images/footer-icon-2.svg'
-import box3 from './asset/images/footer-icon-3.svg'
-import box4 from './asset/images/footer-icon-4.svg'
-import box5 from './asset/images/footer-icon-5.svg'
-import box6 from './asset/images/footer-icon-6.svg'
-import box7 from './asset/images/footer-icon-7.svg'
-import box8 from './asset/images/footer-icon-8.svg'
+// import box1 from './asset/images/footer-icon-1.svg'
+// import box2 from './asset/images/footer-icon-2.svg'
+// import box3 from './asset/images/footer-icon-3.svg'
+// import box4 from './asset/images/footer-icon-4.svg'
+// import box5 from './asset/images/footer-icon-5.svg'
+// import box6 from './asset/images/footer-icon-6.svg'
+// import box7 from './asset/images/footer-icon-7.svg'
+// import box8 from './asset/images/footer-icon-8.svg'
+import footer from './asset/images/footer.svg'
 
 
 const MotionBox = motion(Box)
@@ -22,49 +23,51 @@ function App() {
 
  
   return (
-    <Box bg='brand.900' w={['100vw']} h={['100%','100%']}>
-      <Flex justifyContent='center' py={10} flex='1'>
-        <Image src={logo}  margin={0}   w='10rem'    />
+    <Box>
+    <Box bg='brand.900' w={['100vw']} h={['100%','100%']} >
+      <Flex justifyContent='center' pt={['35px']} flex='1'>
+        <Image src={logo}  margin={0}   w={['9rem','9.4rem','9.8rem','10.1rem']}    />
       </Flex>
 
-        <Flex justifyContent='center' mt='4rem'>
-          <Flex pos='relative' alignItems='center' flexDir='column'>
+        <Flex justifyContent='center' >
+          <Flex pos='relative' alignItems='center' mt={['3rem','4rem','5rem','6rem','7rem']} flexDir='column'>
             
-        <Heading fontSize={["2.5rem","3rem","4rem"]} textAlign='center' font-weight='700' lineHeight='1.2' color='white' letterSpacing='1'>Lifting the veil, <br/>         for everyone
+        <Heading fontSize={["2.5rem","3rem","3.5rem"]}  fontStyle='normal'
+        px={['2rem','4rem','8rem','16rem','30rem']}
+ textAlign='center' font-weight='bold' lineHeight={['3.2rem','3.2rem','4.2rem','4.2rem']} color='white' letterSpacing='1'>We are lifting the veil, for everyone
 </Heading>
 
           <TwinkleStar
           animate={{ rotate: 360, scale: [0, 1, 1.5, 0, 1], opacity:  [1, 0, 1, 1, 0] }}
           transition={{ ease: "easeOut", duration: 2, yoyo: Infinity }}
           
-          src={star} width={['5','8']} pos='absolute' left={['-5','-8']} />
+          src={star} width={['5','8']} pos='absolute' left={['2rem','4rem','8rem','16rem','30rem']} />
           <TwinkleStar
           animate={{ rotate: 360, scale: [1, 0, 1, 1.5, 0], opacity:  [0, 1, 0, 1, 1] }}
           transition={{ ease: "easeOut", duration: 2, yoyo: Infinity }}
-          src={star} width={['5','8']} pos='absolute' right={['-2','-5']} top='-5' />
+          src={star} width={['5','8']} pos='absolute' right={['2rem','4rem','8rem','16rem','30rem']} top='-5' />
           <TwinkleStar 
           animate={{ rotate: 360, scale: [0, 1, 0, 1, 1.5], opacity:  [1, 0, 1, 0, 1] }}
-          transition={{ ease: "easeOut", duration: 2, yoyo: Infinity }} src={star} width={['5','8']} pos='absolute' right={['2px','2px']} bottom='0' />
+          transition={{ ease: "easeOut", duration: 2, yoyo: Infinity }} src={star} width={['5','8']} pos='absolute' right={['8rem','10rem','14rem','22rem','36rem']} bottom='0' />
           </Flex>
           
         </Flex>
             <Text textAlign='center' 
-            fontSize={["1.3rem","1.5rem","2rem"]} 
-            padding={["1rem","1.5rem","2rem"]} 
-            px={['5','10','0']}
+            fontSize={['.9rem','1.1rem','1.28rem']}
+            px={['2rem','5rem', '5rem','20rem']}
 
             letterSpacing='1.2'
-              lineHeight ='1.2'
-              mt='6'
+              lineHeight ={['1.7rem','1.9rem','2.1rem','2.3rem']}
+              my='24px'
             color='white'
-            >We are taking the power from the employer  and  <Box display={['none','none', 'block']} as='br'/> putting it in your hands.</Text>
+            >Flint is a review community that helps people find great places to work. We want to shift the balance of power from the employers to you. </Text>
 
-            <Flex py='3rem' flex='1' flexDir={['column', 'column','row','row']}
+            <Flex flex='1' flexDir={['column', 'column','row','row']}
               alignItems={['center']}
               justifyContent='center'
-              px={['0','10','0']}
+              px={[0,0,3,0]}
               >
-              <Flex mx={['.4rem']}  
+              <Flex mx={['12px']}  
               width={['90%  ','90%  ','60%  ','35%']}
               //'100%','100%',
               justifyContent='center'
@@ -73,8 +76,10 @@ function App() {
               >
 
             <Input placeholder="email address"
-                     px={['1.8rem','1.9rem','2rem','2rem']}
-                     py={[  '1.6rem', '2.1rem','2.2rem','2.2rem']}
+                     px={['1.1rem','1.3rem','1.6rem','2.2rem']}
+                     py={['1.5rem','1.7rem','1.9rem','2.2rem']}
+
+                    //  py={[  '1.6rem', '2.1rem','2.2rem','2.2rem']}
 
                     // 90%', '90%',
                     width={[ '100%']}
@@ -85,12 +90,14 @@ function App() {
         
         />
         </Flex>
-        <Flex mx='.4rem' width={[ '90%','90%', '45%','25%']} my={2}
+        <Flex mx='12px' width={[ '90%','90%', '45%','20%']} my={2}
         //'100%', '100%',
         justifyContent='center'>
-        <Button bg="primary.900"
-        px={['1.8rem','1.9rem','2rem','2rem']}
-        py={[  '1.6rem', '2.1rem','2.2rem','2.2rem']}
+        <Button bg="#00251C"
+        // px={['1.8rem','1.9rem','2rem','2rem']}
+        // py={[  '1.6rem', '2.1rem','2.2rem','2.2rem']}
+        px={['1.1rem','1.3rem','1.6rem','2.2rem']}
+        py={['1.5rem','1.7rem','1.9rem','2.2rem']}
 
         width={[ '100%']}
         //'90%', '90%',
@@ -106,44 +113,48 @@ function App() {
         Get your powers early
         </Button>
               </Flex>
-            </Flex>  
-
-            <MotionFlex 
-            flexDir='row' 
-            alignItems='baseline' 
-            justifyContent='center'
-            w='100%'
-            px='.4rem'
-            
-            mt={['6rem', "4rem", "1rem"]}
-          bottom={0}
-  >
-                <MotionBox   >
-                  <Image src={box1} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box2} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box3} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box4} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box5} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box6} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box7} />
-                </MotionBox>
-                <MotionBox>
-                <Image src={box8} />
-                </MotionBox>
-              </MotionFlex>    
+            </Flex>
     </Box>
+    <MotionFlex 
+    flexDir='row' 
+    alignItems='baseline' 
+    justifyContent='center'
+    w='100%'
+    h='11.45rem'
+
+    mt={['6rem','6rem','7rem','7rem','auto','auto']}
+    
+    
+    
+    
+>
+        <MotionBox   >
+          <Image src={footer} />
+        </MotionBox>
+        {/* <MotionBox>
+        <Image src={box2} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box3} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box4} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box5} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box6} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box7} />
+        </MotionBox>
+        <MotionBox>
+        <Image src={box8} />
+        </MotionBox> */}
+      </MotionFlex>  
+
+      </Box>
   );
 }
 
